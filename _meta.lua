ab5903fbaf0faca9a694204423d9eefe2396ee4c
jgarcia24n <34949136+jgarcia24n@users.noreply.github.com>
2026-05-10 14:26:22 -0500

    Add version number to _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -3,4 +3,5 @@ return {
     name        = "readaloud",
     fullname    = _("ReadAloud"),
     description = _("Plays EPUB3 Media Overlay audio with synchronized text highlighting."),
+    version = "1.0.0",
 }
